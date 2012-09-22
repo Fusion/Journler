bd20b0a09a37f313957ec5179381ac6f33d5ad7c
Chris F Ravenscroft <github@voilaweb.com>
2012-09-22 01:33:07 -0700

    - Checked Lion/Mountain Lion compatibility, updated SDK - Added Lion full screen support - App is now fully licensed

--- JournlerLicenseManager.m
+++ JournlerLicenseManager.m
@@ -42,18 +42,6 @@
 #import "JournlerLicenseManager.h"
 #import "NSString+JournlerAdditions.h"
 
-static NSArray * BannedLicenseNames()
-{
-	static NSArray *array = nil;
-	if ( array == nil ) 
-	{
-		array = [[NSArray alloc] initWithObjects:
-			@"Special [k]", nil];
-	}
-	
-	return array;
-}
-
 @implementation JournlerLicenseManager
 
 + (id)sharedManager 
@@ -65,67 +53,13 @@ static NSArray * BannedLicenseNames()
 
 - (NSInteger) licenseType
 {
-	NSInteger licenseType;
-	
-	NSString *dLicenseName = [[NSUserDefaults standardUserDefaults] stringForKey:@"LicenseName"];
-	NSString *dLicenseCode = [[NSUserDefaults standardUserDefaults] stringForKey:@"LicenseCode"];
-	
-	if ( dLicenseName == nil || dLicenseCode == nil )
-		licenseType = kJournlerLicenseInvalid;
-	else
-		licenseType = [self licenseTypeForName:dLicenseName 
-				digest:dLicenseCode];
-	
-	return licenseType;
+    // Future implementations may return kJournlerLicenseFull
+    return kJournlerLicenseBeta;
 }
 
 - (NSInteger) licenseTypeForName:(NSString*)licenseName digest:(NSString*)proposedDigest
 {
-	NSInteger myLicense;
-	
-	if ( [BannedLicenseNames() containsObject:licenseName] )
-	{
-		myLicense = kJournlerLicenseInvalid;
-	}
-	else
-	{
-	
-	#ifdef __BETA__
-		
-		NSString *licenseDigestBeta =		[licenseName formattedMD5DigestForLicense:2 version:210];
-		NSString *licenseDigestJournler =	[licenseName formattedMD5DigestForLicense:3 version:210];
-		
-		if ( [[proposedDigest uppercaseString] isEqualToString:[licenseDigestBeta uppercaseString]] )
-			myLicense = kJournlerLicenseBeta;
-		else if ( [[proposedDigest uppercaseString] isEqualToString:[licenseDigestJournler uppercaseString]] )
-			myLicense = kJournlerLicenseSpecial;
-		else
-			myLicense = kJournlerLicenseInvalid;
-		
-	#else 
-		
-		NSString *licenseDigestPersonal =	[licenseName formattedMD5DigestForLicense:0 version:210];
-		NSString *licenseDigestBusiness =	[licenseName formattedMD5DigestForLicense:1 version:210];
-		NSString *licenseDigestJournler =	[licenseName formattedMD5DigestForLicense:3 version:210];
-		
-		NSString *licenseDigestFull =		[licenseName formattedMD5DigestForLicense:9 version:260];
-		
-		if ( [[proposedDigest uppercaseString] isEqualToString:[licenseDigestPersonal uppercaseString]] )
-			myLicense = kJournlerLicensePersonal;
-		else if ( [[proposedDigest uppercaseString] isEqualToString:[licenseDigestBusiness uppercaseString]] )
-			myLicense = kJournlerLicenseNonPersonal;
-		else if ( [[proposedDigest uppercaseString] isEqualToString:[licenseDigestJournler uppercaseString]] )
-			myLicense = kJournlerLicenseSpecial;
-		else if ( [[proposedDigest uppercaseString] isEqualToString:[licenseDigestFull uppercaseString]] )
-			myLicense = kJournlerLicenseFull;
-		else
-			myLicense = kJournlerLicenseInvalid;
-		
-	#endif
-
-	}
-	
-	return myLicense;
+    return [self licenseType];
 }
 
 @end
